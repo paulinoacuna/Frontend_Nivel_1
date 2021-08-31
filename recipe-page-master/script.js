7973b70a34f560f59605a87b18aa7123a38642f2
paulinoacuna <paulinomaven911@gmail.com>
2021-08-30 21:51:13 -0500

    mobile & responsive design

--- recipe-page-master/script.js
+++ recipe-page-master/script.js
@@ -1,14 +1,20 @@
+if (window.screen.width < 976) {
+    let vector = document.getElementById("svg1")
+    vector.setAttribute("width", "60")
+
+}
+
 function validar() {
     var check1 = document.getElementById("1").checked
     var check2 = document.getElementById("2").checked
     var check3 = document.getElementById("3").checked
-    
+
     if (check1 && check2 && check3) {
         alert("Genial, Tienes los ingredientes completos!")
     }
 }
 
-function validar2(){
+function validar2() {
     var check4 = document.getElementById("4").checked
     var check5 = document.getElementById("5").checked
     var check6 = document.getElementById("6").checked
@@ -16,9 +22,9 @@ function validar2(){
     var check8 = document.getElementById("8").checked
     var check9 = document.getElementById("9").checked
     var check10 = document.getElementById("10").checked
-    
+
     if (check4 && check5 && check6 && check7 && check8 && check9 && check10) {
-    alert("Genial, Tienes los ingredientes completos!")
+        alert("Genial, Tienes los ingredientes completos!")
     }
-    
+
 }
